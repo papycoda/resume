0349c91328f6c3e9daa7930ff63c257e3fbfd7e7
Papycoda <opeyemi655@gmail.com>
2021-08-20 20:12:46 +0100

    Initial commit

---
+++ resume/resumeapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def index(request):
+    # if request.method == 'POST':
+    #     message = request.POST['message']
+    #
+    #     send_mail('Contact Form',
+    #               message,
+    #               settings.EMAIL_HOST_USER,
+    #               ['opeyemi655@gmail.com'],
+    #               fail_silently=False)
+    return render(request, 'index.html')
